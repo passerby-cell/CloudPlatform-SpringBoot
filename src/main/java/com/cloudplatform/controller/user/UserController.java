3c10fc892ee4e642f8327a549cd411bc227b933e
lhy <18606145168@163.com>
2022-06-21 10:42:43 +0800

    init utils

---
+++ src/main/java/com/cloudplatform/controller/user/UserController.java
@@ -0,0 +1,18 @@
+package com.cloudplatform.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 林海洋
+ * @Date: 2022/06/21/10:31 上午
+ * @Description:
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+   // @GetMapping("login")
+
+}
