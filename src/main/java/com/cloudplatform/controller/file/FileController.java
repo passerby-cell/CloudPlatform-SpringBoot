4b12d92985f069800d51cc3023efbc76c86f9455
lhy <18606145168@163.com>
2022-06-23 14:28:49 +0800

    初始化文件

---
+++ src/main/java/com/cloudplatform/controller/file/FileController.java
@@ -0,0 +1,16 @@
+package com.cloudplatform.controller.file;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 林海洋
+ * @Date: 2022/06/23/2:28 下午
+ * @Description:
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+}
